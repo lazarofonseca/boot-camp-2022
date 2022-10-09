d26adb1c36eea1be31bfe4f34a51c2c07685162d
Lázaro <lazaronobrega3@gmail.com>
2022-10-09 19:37:23 -0300

    Create project

---
+++ backend/dscatalog/src/test/java/com/lazaro/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazaro.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
